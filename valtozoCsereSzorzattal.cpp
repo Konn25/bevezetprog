524af31ce837c8ad8e1a50c0f8dafdad70fc5dc0
Konn25 <34648196+Konn25@users.noreply.github.com>
2019-09-21 15:19:19 +0200

    Add files via upload

---
+++ valtozoCsereSzorzattal.cpp
@@ -0,0 +1,19 @@
+#include "std_lib_facilities.h"
+
+int main(){
+	
+	int a=7;
+	int b=5;
+	cout<<"Eredeti ertekek: \n";
+	cout<<"a: "<<a<<" b: "<<b;
+	cout<<"\n";
+	a=b*a;
+	b=a/b;
+	a=a/b;
+	
+	cout<<"Megcserelt ertekek: \n";
+	cout<<"a: "<<a<<" b: "<<b;
+	
+	
+	
+}
