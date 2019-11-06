8859568409b00ecc20df5f0602eba68448948596
Konn25 <34648196+Konn25@users.noreply.github.com>
2019-11-06 12:04:01 +0100

    Add files via upload

---
+++ Drill8/my.cpp
@@ -0,0 +1,14 @@
+#include "my.h"
+#include "std_lib_facilities.h"
+
+using namespace std;
+int foo;
+
+void print_foo(){
+    cout<<foo;
+}
+
+void print(int _i){
+    cout<<_i;
+
+}
---
+++ Drill8/use.cpp
@@ -0,0 +1,14 @@
+#include "my.h"
+
+int main(){
+    foo=7;
+    int k=99;
+    print_foo();
+    print(99);
+
+    char cc;
+    cin >> cc;
+
+    return 0;
+
+}
